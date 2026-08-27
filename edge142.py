5850ec435e125bbbd94ef6943b07651677412c1f
Shahed Prithibi <shahedshahriarprithibi@gmail.com>
2026-08-28 00:35:33 +0600

    Build basic Edge142 interaction loop

---
+++ edge142.py
@@ -0,0 +1,11 @@
+while True:
+    message = input()
+
+    if message == "hello buddy":
+      print("Hello, I'm Edge142. Your personal Ai companion")
+
+    elif message == "bye":
+      print("Bye, Sir. Have a good day.")
+
+    else:
+      print("I am not sure if i understand that yet")
