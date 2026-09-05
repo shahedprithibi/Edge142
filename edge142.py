89e081d5a65bf5f69cc3556cdfe8696c87dc29ee
Shahed Prithibi <shahedshahriarprithibi@gmail.com>
2026-09-05 20:18:33 +0600

    Add basic intent detection

--- edge142.py
+++ edge142.py
@@ -1,14 +1,27 @@
+def detect_intent(message):
+    if "hello" in message or "hey" in message: 
+        return "greeting"
+
+    elif "bye" in message or "goodbye" in message:
+        return "farewell"
+
+    else:
+        return "unknown"
+
 
 def respond(message):
 
-    if message == "hello buddy":
-        return "Hello, Prithibi. how may I help you ?"
+    intent = detect_intent(message)
 
-    elif message == "bye":
+    if intent == "greeting":
+        return "Hello, Prithibi. How may I help you?"
+
+    elif intent == "farewell":
         return "Bye, Sir. Have a good day."
 
     else:
-        return "I am not sure if i understand that yet"
+        return "I am not sure if I understand that yet"
+
 
 while True:
     message = input()
